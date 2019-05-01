9cac0282b85203fcbd490f41657c25b6b036001c
Justin Hull <hulljc3@upmc.edu>
2019-04-30 23:39:29 -0400

    Fix Dependencies

--- src/app/landing/landing.component.spec.ts
+++ src/app/landing/landing.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingComponent } from './landing.component';
+import {MaterialModule} from "../shared/material/material.module";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
@@ -8,7 +10,8 @@ describe('LandingComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LandingComponent ]
+      declarations: [ LandingComponent ],
+      imports: [ RouterTestingModule, MaterialModule]
     })
     .compileComponents();
   }));
